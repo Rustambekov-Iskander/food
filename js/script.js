afb41583fef09ff4c4bb1f88a9388682c03868fc
Iskander <isken.kgz.2005@gmail.com>
2022-02-20 10:52:21 +0600

    added timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+    // Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -37,6 +39,61 @@ window.addEventListener('DOMContentLoaded', () => {
             }
 
         } );
+
+
+        // Timer
+        const deadline = '2022-02-24';
+
+        function getTimeRemaining(endTime){
+            const t = Date.parse(endTime) - Date.parse(new Date()),
+                  days = Math.floor( t / ( 1000 * 60 * 60 * 24 ) ),
+                  hours = Math.floor( ( t / ( 1000 * 60 * 60 ) ) % 24 ),
+                  minutes = Math.floor( ( t / ( 1000 * 60 ) ) % 60 ),
+                  seconds = Math.floor( ( t /  1000 ) % 60 );
+            
+            return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+        }
+
+        function setClock(selection, endTime){
+            const timer = document.querySelector(selection),
+                  days = document.querySelector('#days'),
+                  hours = document.querySelector('#hours'),
+                  minutes = document.querySelector('#minutes'),
+                  seconds = document.querySelector('#seconds'),
+                  timeInterval = setInterval(updateClock, 1000);
+            updateClock();
+            
+            function updateClock(){
+                const t = getTimeRemaining(endTime);
+
+                function addZero(element, getR){
+                    if ( getR < 10 ){
+                        element.innerHTML = `0${getR}`;
+                    }else{
+                        element.innerHTML = getR;
+                    }
+                }
+                addZero(days, t.days);
+                addZero(hours, t.hours);
+                addZero(minutes, t.minutes);
+                addZero(seconds, t.seconds);
+                
+
+                if (t.total <= 0){
+                    clearInterval(timeInterval);
+                }
+            }
+        }
+
+        setClock('.timer', deadline);
+
+
 
 
 });
